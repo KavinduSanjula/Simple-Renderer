ec99f435f402e022dd0af22b8b72febf83bd92bc
kavindusanjula <kavindusanjula.1@gmail.com>
2022-06-10 22:15:05 +0530

    Starting Project

--- Simple Renderer/src/Main.cpp
+++ Simple Renderer/src/Main.cpp
@@ -1,6 +1,36 @@
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
+#include "core/opengl.h"
+
+#include <iostream>
 
 int main() {
+	GLFWwindow* window;
+	if (!glfwInit())
+		return -1;
+
+	window = glfwCreateWindow(800, 600, "Simple Renderer", NULL, NULL);
+	if (!window) {
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glewInit();
+	std::cout << "OpenGL Version : " << glGetString(GL_VERSION);
+
+	while(!glfwWindowShouldClose(window)){
+
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f( 0.5f, -0.5f);
+		glVertex2f( 0.0f,  0.5f);
+		glEnd();
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
---
+++ Simple Renderer/src/core/opengl.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define ASSERT(x) if(!(x)) __debugbreak()
